d370581d9e9ffe7b76f831fa455c80f21f21dc69
SergeyLubash <luba-sergei@yandex.ru>
2022-12-22 17:27:33 +0300

    Add goals/

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -45,13 +45,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'core',
-
     #third-party apps
     'rest_framework',
     'corsheaders',
     'social_django',
+    'django_filters',
+    'core',
+    'goals',
 ]
 
 MIDDLEWARE = [
@@ -160,3 +160,11 @@ SOCIAL_AUTH_URL_NAMESPACE = 'social'
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/categories'
 # SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/logged-in/'
 SOCIAL_AUTH_LOGIN_ERROR_URL = '/login-error/'
+
+
+# REST_FRAMEWORK
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+}
